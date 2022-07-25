72cf3d1ae3b9c8c5b01031263e09c0fb119e5ce6
hasib2019 <hasib.9437.hu@gmail.com>
2022-07-25 23:56:40 +0600

    fb chat plugin added

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -143,6 +143,38 @@
     {{-- <link href="{{ asset('frontend/css/fb-style.css')}}" rel="stylesheet"> --}}
     <!--facebook pixel-->
     <!-- Meta Pixel Code -->
+    {{-- facebook c8 plugin  --}}
+    <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Chat Plugin code added by Hasib -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "104928051605250");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v14.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
+    {{-- facebook chat plugin end added by Hasib --}}
     <script>
         ! function(f, b, e, v, n, t, s) {
             if (f.fbq) return;
